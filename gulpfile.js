d948c648edfaa10f12f2c7d4036e83f2b68a7356
jb <jb@remotium.com>
2015-05-07 23:01:25 -0700

    clean package.json
    remove useless sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,4 @@
 var gulp = require('gulp');
-var sourcemaps = require('gulp-sourcemaps');
 var babel = require('gulp-babel');
 
 var path = require('path');
@@ -8,16 +7,12 @@ var path = require('path');
 var paths = {
     src: ['src/**/*.js'],
     build: 'build',
-    souceRoot: path.join(__dirname, 'src')
+    sourceRoot: path.join(__dirname, 'src')
 };
 
 gulp.task('build', function(){
     return gulp.src(paths.src)
-        .pipe(sourcemaps.init())
         .pipe(babel())
-        .pipe(sourcemaps.write('.'), {
-            //sourceRoot: paths.souceRoot
-        })
         .pipe(gulp.dest(paths.build));
 });
 
